cfafed3fa3ff61a5ff6f2c11b670108ab8e4df7f
xiede000000 <2631661934@qq.com>
2017-03-17 00:14:49 +0800

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-
+use Auth;
 class UsersController extends Controller
 {
     public function create()
@@ -33,6 +33,7 @@ class UsersController extends Controller
            'email' => $request->email,
            'password' => bcrypt($request->password),
        ]);
+       Auth::login($user);
        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
        return redirect()->route('users.show', [$user]);
     }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,7 @@ get('/about', 'StaticPagesController@about')->name('about');
 
 get('signup', 'UsersController@create')->name('signup');
 resource('users', 'UsersController');
+
+get('login', 'SessionsController@create')->name('login');
+post('login', 'SessionsController@store')->name('login');
+delete('logout', 'SessionsController@destroy')->name('logout');
