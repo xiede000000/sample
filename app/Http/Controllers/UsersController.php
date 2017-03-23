cf070e37360f511014e0268e7b5dc3ca09a74d2c
xiede000000 <2631661934@qq.com>
2017-03-24 00:39:20 +0800

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -38,4 +38,56 @@ class UsersController extends Controller
        return redirect()->route('users.show', [$user]);
     }
 
+    public function edit($id)
+    {
+      $user = User::findOrFail($id);
+      $this->authorize('update', $user);
+      return view('users.edit', compact('user'));
+    }
+
+    public function update($id, Request $request)
+    {
+      $this->validate($request,[
+        'name'=>'required|max:50',
+        'password'=>'required|confirmed|min:6'
+      ]);
+      $user = User::findOrFail($id);
+      $this->authorize('update', $user);
+
+      $data= [];
+      $data['name'] = $request->name;
+      if ($request->password) {
+          $data['password'] = bcrypt($request->password);
+      }
+
+      $user->update($data);
+
+      session()->flash('success', '个人资料更新成功！');
+
+      return redirect()->route('users.show', $id);
+    }
+
+    public function __construct()
+    {
+      $this->middleware('auth', ['only'=> ['edit', 'update', 'destroy']
+                      ]);
+      $this->middleware('guest', [
+                    'only' => ['create']
+            ]);
+    }
+
+    public function index()
+    {
+      $users = User::paginate(30);
+      return view('users.index', compact('users'));
+    }
+
+  public function destroy($id)
+   {
+       $user = User::findOrFail($id);
+       $this->authorize('destroy', $user);
+       $user->delete();
+       session()->flash('success', '成功删除用户！');
+       return back();
+   }
 }
